9e0316a734107feeb4dd5afe03e7ba303e9a5801
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-09-21 21:29:35 -0400

    MadeSomeProgress

---
+++ Assets/Stephen_Assets/Stephen_Scripts/HealthbarScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthbarScript : MonoBehaviour
+{
+    public Slider healthslider;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMaxHealth(int playerHealth)
+    {
+        healthslider.maxValue = playerHealth;
+        healthslider.value = playerHealth;
+    }
+
+    public void SetPlayerHealth(int playerHealth)
+    {
+        healthslider.value = playerHealth;
+
+
+    }
+}
