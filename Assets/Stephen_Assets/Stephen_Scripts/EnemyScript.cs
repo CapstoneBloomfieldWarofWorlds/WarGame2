d23006becb54c6b807a4642ab63865d34303e444
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-09-25 19:11:22 -0400

    Created a rough shootingScript

---
+++ Assets/Stephen_Assets/Stephen_Scripts/EnemyScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    private float health = 40;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        health -= damageAmount;
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }  
+    }
+}
---
+++ Assets/Stephen_Assets/Stephen_Scripts/ShootingScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public float raylength = 1f;
+
+    private float damage = 5f;
+
+    private RaycastHit objecthit;
+
+    public LayerMask EnemyMask;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.forward * raylength, Color.blue);
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+       
+    }
+
+    void Shoot()
+    {
+        if(Physics.Raycast(transform.position, transform.forward, out objecthit, raylength, EnemyMask))
+        {
+            print("I hit " + objecthit.collider.gameObject.name);
+
+            EnemyScript enemy = objecthit.transform.GetComponent<EnemyScript>();
+
+            if(enemy != null)
+            {
+                enemy.TakeDamage(damage);
+            }
+        }
+        else
+        {
+            print("Missed!");
+        }
+
+        
+    }
+}
