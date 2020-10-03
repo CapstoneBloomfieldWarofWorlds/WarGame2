7192e986e92748852e3eaae2ab3e24e6898d933c
TheSteamingArtist <50603055+TheSteamingArtist@users.noreply.github.com>
2020-10-02 22:01:09 -0400

    Made a rough Crouch

---
+++ Assets/Stephen_Assets/Stephen_Scripts/CrouchScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrouchScript : MonoBehaviour
+{
+    CharacterController controller;
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.C))
+        {
+            controller.height = 1.0f;
+
+        }
+
+        else
+        {
+            controller.height = 1.8f;
+        }
+    }
+}
